564d82dc74f2f115153ec48d49e41dd32ce70f3d
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2022-05-15 23:31:57 +0900

    login実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   namespace :admin do
     resources :users
   end
