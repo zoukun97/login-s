276c26ff15733109582ae57c89a87cfd7ed03c9f
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2022-05-15 23:34:43 +0900

    リダイレクト先変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'ログインしました。'
+      redirect_to admin_user_path(user), notice: 'ログインしました。'
     else
       render :new
     end
